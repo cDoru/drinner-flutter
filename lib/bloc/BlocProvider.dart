ab5ff6c25ee5208de4603aaf94af7ae28b741000
tomwyr <tomwyr@hotmail.com>
2018-09-14 21:35:42 +0200

    Create BlocProvider + remove default test

---
+++ lib/bloc/BlocProvider.dart
@@ -0,0 +1,36 @@
+import 'package:drinner_flutter/bloc/BaseBloc.dart';
+import 'package:flutter/material.dart';
+
+class BlocProvider<T extends BaseBloc> extends StatefulWidget {
+  BlocProvider({
+    Key key,
+    @required this.child,
+    @required this.bloc,
+  });
+
+  final Widget child;
+
+  final T bloc;
+
+  static T of<T extends BaseBloc>(BuildContext context) {
+    final type = _typeOf<BlocProvider<T>>();
+    final provider = context.ancestorWidgetOfExactType(type) as BlocProvider<T>;
+    return provider.bloc;
+  }
+
+  static Type _typeOf<T>() => T;
+
+  @override
+  State<StatefulWidget> createState() => _BlocProviderState();
+}
+
+class _BlocProviderState extends State<BlocProvider> {
+  @override
+  void dispose() {
+    widget.bloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.child;
+}

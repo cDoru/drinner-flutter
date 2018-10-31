4c1b4d4bf854955f50ffc40b27f1cd1b93d0a5b3
tomwyr <tomwyr@hotmail.com>
2018-10-31 10:21:16 +0100

    Add AppConfig to repo

---
+++ lib/app/AppConfig.dart
@@ -0,0 +1,5 @@
+class AppConfig {
+  static const String MAPBOX_TOKEN = '<mapbox_token>';
+  static const String MAPBOX_URL = '<mapbox_url>';
+  static const String API_BASE_URL = '<api_base_url>';
+}
